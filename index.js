752b9aed80b36af347509fc3f59808fbb7d64055
Blue Bloob <bluebloob@Blues-MacBook-Air.local>
2017-03-04 17:30:57 -0800

    Updated to the personalized version of repo redy for github

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', function(request, response) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('Running on port:', app.get('port'));
 });
 
 
